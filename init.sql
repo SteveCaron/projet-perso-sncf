41a799acc4468c88daa1d644f5946f5281d6786b
SteveCaron <steve.caron.59@gmail.com>
2024-02-15 15:28:46 +0100

    creation de la table arrivee avec instanciation du conteneur

---
+++ init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE arrivees (
+	arrivee_id VARCHAR(255) PRIMARY KEY NOT NULL, 
+	gare_id VARCHAR(255), 
+	departure_date_time DATETIME, 
+	base_departure_date_time DATETIME, 
+	arrival_date_time DATETIME, 
+	base_arrival_date_time DATETIME, 
+	network VARCHAR(255), 
+	ligne VARCHAR(255), 
+	trip VARCHAR(255), 
+	direction VARCHAR(255), 
+	disruption_id VARCHAR(255)
+)
