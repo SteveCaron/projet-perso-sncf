af46f43f71137ec662fc45a497011f178ce72cd7
SteveCaron <steve.caron.59@gmail.com>
2024-02-26 14:40:13 +0100

    creation table et foreign keys

--- init.sql
+++ init.sql
@@ -1,3 +1,24 @@
+CREATE TABLE IF NOT EXISTS gares (
+	id VARCHAR(255) PRIMARY KEY NOT NULL,
+	nom VARCHAR(255),
+	timezone VARCHAR(255),
+	label VARCHAR(255),
+	longitude DECIMAL(11,8),
+	latitude DECIMAL(10,8),
+	id_region_ad VARCHAR(255),
+	nom_region_ad VARCHAR(255),
+	zip_code VARCHAR(255),
+	insee_region_ad VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS perturbations (
+	id VARCHAR(255) PRIMARY KEY NOT NULL,
+	debut DATETIME,
+	fin DATETIME,
+	effet VARCHAR(255),
+	message_display VARCHAR(255)
+);
+
 CREATE TABLE IF NOT EXISTS arrivees (
 	id VARCHAR(255) PRIMARY KEY NOT NULL, 
 	gare_id VARCHAR(255), 
@@ -9,13 +30,7 @@ CREATE TABLE IF NOT EXISTS arrivees (
 	ligne VARCHAR(255), 
 	trip VARCHAR(255), 
 	direction VARCHAR(255), 
-	disruption_id VARCHAR(255)
-);
-
-CREATE TABLE IF NOT EXISTS perturbations (
-	perturbation_id VARCHAR(255) PRIMARY KEY NOT NULL,
-	debut DATETIME,
-	fin DATETIME,
-	effet VARCHAR(255),
-	message_display VARCHAR(255)
+	perturbation_id VARCHAR(255),
+	FOREIGN KEY (perturbation_id) REFERENCES perturbations(id),
+	FOREIGN KEY (gare_id) REFERENCES gares(id)
 );
